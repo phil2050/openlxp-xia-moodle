cd3e84cc9fee49cb61b2581d89891ab484fa500a
Jean Diouf <jdiouf@deloitte.com>
2021-03-16 14:29:45 -0400

    removed default sk

--- app/openlxp_xia_jko_project/settings.py
+++ app/openlxp_xia_jko_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's7*8r51e^s8n_ck=4am1gdozq3i0ayb0kwe=v@@4600rm=(opi'
+SECRET_KEY = os.environ.get('SECRET_KEY_VAL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
