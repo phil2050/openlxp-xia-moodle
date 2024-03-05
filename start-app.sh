1b93196a7173f18fc3a7347e6c153a6522df0e0c
kjijo <kjijo@deloitte.com>
2024-03-05 13:00:11 -0600

    Adding Cachetable

--- start-app.sh
+++ start-app.sh
@@ -3,6 +3,7 @@
 
 python manage.py waitdb 
 python manage.py migrate 
+python manage.py createcachetable 
 python manage.py loaddata admin_theme_data.json
 cd /opt/app/ 
 if [ -n "$TMP_SOURCE_DIR" ] ; then 
