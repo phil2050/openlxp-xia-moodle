1d18f6f21ab635d65e34bf31cd6d3257878ce1b1
KarenAJ <kjijo@deloitte.com>
2024-10-23 12:09:08 -0500

    Update start-app.sh

--- start-app.sh
+++ start-app.sh
@@ -5,6 +5,7 @@ python manage.py waitdb
 python manage.py migrate 
 python manage.py createcachetable 
 python manage.py loaddata admin_theme_data.json 
+python manage.py loaddata openlxp_email.json 
 python manage.py loaddata openlxp_notification_template.json 
 python manage.py loaddata openlxp_notification_subject.json 
 python manage.py loaddata openlxp_notification_email.json 
